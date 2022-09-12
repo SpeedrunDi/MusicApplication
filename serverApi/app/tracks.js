2a9839b8f2fd9ae96cd5bf988f8e0f2a3cd06355
SpeedrunDi <amanmaksat0@gmail.com>
2022-09-12 15:18:33 +0600

    made GET request on all tracks of one artist.

--- serverApi/app/tracks.js
+++ serverApi/app/tracks.js
@@ -1,20 +1,38 @@
 const express = require('express');
 const Track = require('../models/Track');
+const Album = require('../models/Album');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
   const query = {};
-  if (req.query.album) {
-    query.album = {$eq: req.query.album};
-  }
-
   try {
-    const tracks = await Track
-      .find(query)
-      .populate('album', 'title');
+    if (req.query.artist) {
+      query.artist = {$eq: req.query.artist};
 
-    res.send(tracks);
+      let albumsObj;
+      albumsObj = await Album.find(query, {_id: 1});
+      let albumsId;
+      if (albumsObj) {
+        albumsId = albumsObj.map(album => album._id);
+      }
+
+      const tracks = await Track
+        .find({$in: albumsId})
+        .populate('album', 'title');
+
+      res.send(tracks);
+    } else {
+      if (req.query.album) {
+        query.album = {$eq: req.query.album};
+      }
+
+      const tracks = await Track
+        .find(query)
+        .populate('album', 'title');
+
+      res.send(tracks);
+    }
   } catch {
     res.sendStatus(500);
   }
